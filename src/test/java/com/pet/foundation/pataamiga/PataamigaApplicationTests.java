ebc32a6805fe2697b0cc0828e9590da83c0fd8a1
MatheusVict <matheusvictorhenrique@gmail.com>
2023-09-07 21:41:14 -0300

    feat: initial commit
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

---
+++ src/test/java/com/pet/foundation/pataamiga/PataamigaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.foundation.pataamiga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PataamigaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
