13785f54d210cda5bd36c43806bc1c0c8be239bf
MatheusVict <matheusvictorhenrique@gmail.com>
2023-09-18 15:03:36 -0300

    test: repository tests
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/test/java/com/pet/foundation/pataamiga/reposiotries/UserRepositoryTest.java
+++ src/test/java/com/pet/foundation/pataamiga/reposiotries/UserRepositoryTest.java
@@ -1,17 +1,21 @@
 package com.pet.foundation.pataamiga.reposiotries;
 
 import com.pet.foundation.pataamiga.domain.user.User;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @DisplayName("UserRepository tests")
+@Log4j2
 class UserRepositoryTest {
 
     @Autowired
@@ -25,6 +29,31 @@ class UserRepositoryTest {
                 1L,
                 "uuid",
                 "name",
+                "email1",
+                "password",
+                null,
+                null,
+                null,
+                null,
+                null
+        ));
+
+        // given
+        String uuid = userRepository.findAll().get(0).getUuid();
+        // when
+        Optional<User> user = userRepository.findByUuid(uuid);
+        // then
+        log.info(user.get().getUuid());
+        assertEquals(uuid, user.get().getUuid());
+    }
+
+    @Test
+    @DisplayName("Test findByEmail")
+    void should_ReturnUserByEmail_When_Success() {
+        userRepository.save(new User(
+                1L,
+                "uuid",
+                "name",
                 "email",
                 "password",
                 null,
@@ -34,11 +63,11 @@ class UserRepositoryTest {
                 null
         ));
         // given
-        String uuid = "uuid";
+        String email = "email";
         // when
-        var user = userRepository.findByUuid(uuid);
+        var user = userRepository.findByEmail(email);
         // then
-        assertEquals(uuid, user.get().getUuid());
+        assertEquals(email, user.getEmail());
     }
 
 }
