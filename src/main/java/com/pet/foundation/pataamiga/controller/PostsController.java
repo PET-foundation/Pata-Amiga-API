21ea8e0d2a8c943952e7aa536c1df5798e662234
MatheusVict <matheusvictorhenrique@gmail.com>
2023-09-20 13:43:40 -0300

    feat: controller swagger docs
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/main/java/com/pet/foundation/pataamiga/controller/PostsController.java
+++ src/main/java/com/pet/foundation/pataamiga/controller/PostsController.java
@@ -5,6 +5,8 @@ import com.pet.foundation.pataamiga.domain.posts.Posts;
 import com.pet.foundation.pataamiga.domain.posts.dto.PostCreateDTO;
 import com.pet.foundation.pataamiga.domain.posts.dto.PostUpdateDTO;
 import com.pet.foundation.pataamiga.service.PostsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,39 +22,53 @@ public class PostsController {
     private final PostsService postsService;
 
     @GetMapping
+    @Operation(summary = "Get all posts")
+    @Tag(name = "Posts")
     public ResponseEntity<List<PostsListResponse>> getAllPosts() {
         List<PostsListResponse> response = PostsListResponse.toResponse(postsService.findAll());
         return ResponseEntity.ok(response);
     }
 
     @GetMapping("/{uuid}")
+    @Operation(summary = "Get post by uuid")
+    @Tag(name = "Posts")
     public ResponseEntity<Posts> getPostByUuid(@PathVariable String uuid) {
         return ResponseEntity.ok(postsService.findByUuid(uuid));
     }
 
     @GetMapping("/user")
+    @Operation(summary = "Get posts by user uuid")
+    @Tag(name = "Posts")
     public ResponseEntity<List<Posts>> getPostsByUserUuid(@RequestParam String userUuid) {
         return ResponseEntity.ok(postsService.findByUserUuid(userUuid));
     }
 
     @GetMapping("/name")
+    @Operation(summary = "Get posts by name")
+    @Tag(name = "Posts")
     public ResponseEntity<List<Posts>> getPostsByName(@RequestParam String nameContains) {
         return ResponseEntity.ok(postsService.containsName(nameContains));
     }
 
     @PostMapping
+    @Operation(summary = "Create post")
+    @Tag(name = "Posts")
     public ResponseEntity<?> createPost(@RequestBody PostCreateDTO post) {
         postsService.save(post);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/{uuid}")
+    @Operation(summary = "Update post")
+    @Tag(name = "Posts")
     public ResponseEntity<?> updatePost(@PathVariable String uuid, @RequestBody PostUpdateDTO post) {
         postsService.update(uuid, post);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping
+    @Operation(summary = "Delete post")
+    @Tag(name = "Posts")
     public ResponseEntity<?> deletePost(@RequestParam String uuid) {
         postsService.delete(uuid);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
--- src/main/java/com/pet/foundation/pataamiga/controller/UserController.java
+++ src/main/java/com/pet/foundation/pataamiga/controller/UserController.java
@@ -6,6 +6,8 @@ import com.pet.foundation.pataamiga.domain.user.dto.UserCreateDTO;
 import com.pet.foundation.pataamiga.domain.user.dto.UserResponseDTO;
 import com.pet.foundation.pataamiga.domain.user.dto.UserUpdateDTO;
 import com.pet.foundation.pataamiga.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,6 +24,8 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/{uuid}")
+    @Operation(summary = "Get user by uuid")
+    @Tag(name = "user")
     public ResponseEntity<UserResponseDTO> getUserByUuid(@PathVariable String uuid) {
         User user = userService.getUserByUuid(uuid);
         UserResponseDTO response = UserResponseDTO.toResponse(user);
@@ -29,18 +33,24 @@ public class UserController {
     }
 
     @PostMapping
+    @Operation(summary = "Create user")
+    @Tag(name = "user")
     public ResponseEntity<UserCreatedResponse> createUser(@RequestBody @Valid UserCreateDTO user) {
         String userSaved = userService.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(new UserCreatedResponse(userSaved));
     }
 
     @PutMapping("/{uuid}")
+    @Operation(summary = "Update user")
+    @Tag(name = "user")
     public ResponseEntity<User> updateUser(@PathVariable String uuid, @RequestBody @Valid UserUpdateDTO user) {
         userService.updateUserByUuid(uuid, user);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{uuid}")
+    @Operation(summary = "Delete user")
+    @Tag(name = "user")
     public ResponseEntity<User> deleteUser(@PathVariable String uuid) {
         userService.deleteUserByUuid(uuid);
         return ResponseEntity.ok().build();
