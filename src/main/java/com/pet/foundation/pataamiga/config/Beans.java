3e8203545ba4c842fe02614b7fc400201d3c8ee7
MatheusVict <matheusvictorhenrique@gmail.com>
2023-09-22 10:43:10 -0300

    chore: adding swagger authorization configs
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/main/java/com/pet/foundation/pataamiga/config/Beans.java
+++ src/main/java/com/pet/foundation/pataamiga/config/Beans.java
@@ -1,10 +1,12 @@
 package com.pet.foundation.pataamiga.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,10 +29,22 @@ public class Beans {
         return RedisCacheManager.builder(factory).cacheDefaults(config).build();
     }
 
+    public SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
 
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes(
+                        "Bearer Authentication",
+                        createAPIKeyScheme()
+                ))
                 .info(new Info().title("Pata Amiga")
                         .description("API for to take care of homeless animals")
                         .version("1.0").contact(new Contact().name("Matheus Victor")
