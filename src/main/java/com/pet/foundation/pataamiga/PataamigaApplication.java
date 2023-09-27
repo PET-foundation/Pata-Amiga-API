d424336c6ce8bf11ab1bc432e70236f2d8b2522a
MatheusVict <matheusvictorhenrique@gmail.com>
2023-09-27 20:49:56 -0300

    fix: cors
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/main/java/com/pet/foundation/pataamiga/PataamigaApplication.java
+++ src/main/java/com/pet/foundation/pataamiga/PataamigaApplication.java
@@ -1,11 +1,14 @@
 package com.pet.foundation.pataamiga;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server url")})
 public class PataamigaApplication {
 
 	public static void main(String[] args) {
