4e5622e0be10bf8b24dd9937468f619fdd044598
MatheusVict <matheusvictorhenrique@gmail.com>
2023-09-27 21:15:01 -0300

    fix: security forbidden
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- src/main/java/com/pet/foundation/pataamiga/config/security/SecurityConfigurations.java
+++ src/main/java/com/pet/foundation/pataamiga/config/security/SecurityConfigurations.java
@@ -38,8 +38,8 @@ public class SecurityConfigurations {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(AUTH_SWAGGER_WHITELIST).permitAll()
-                        .requestMatchers(HttpMethod.POST, "api/v1/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "api/v1/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
