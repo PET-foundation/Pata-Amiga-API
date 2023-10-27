8557757a746717264df3a74588c053e6b75f95e8
MatheusVict <matheusvictorhenrique@gmail.com>
2023-10-27 17:06:47 -0300

    feat: adoption entity

---
+++ src/main/java/com/pet/foundation/pataamiga/domain/adoption/Adoption.java
@@ -0,0 +1,47 @@
+package com.pet.foundation.pataamiga.domain.adoption;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.pet.foundation.pataamiga.domain.posts.Posts;
+import com.pet.foundation.pataamiga.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "adoptions")
+public class Adoption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "user_id")
+    @JsonIgnore
+    private User adopter;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "post_id")
+    @JsonIgnore
+    private Posts adopted;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
