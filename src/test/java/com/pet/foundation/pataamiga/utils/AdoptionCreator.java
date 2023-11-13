cc12e583138917b377d7ff5006e18ca7e1d5a91f
MatheusVict <matheusvictorhenrique@gmail.com>
2023-11-13 13:45:52 -0300

    test: adoption tests

--- src/test/java/com/pet/foundation/pataamiga/utils/AdoptionCreator.java
+++ src/test/java/com/pet/foundation/pataamiga/utils/AdoptionCreator.java
@@ -4,6 +4,7 @@ import com.pet.foundation.pataamiga.domain.adoption.Adoption;
 
 import java.sql.Date;
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class AdoptionCreator {
@@ -11,11 +12,11 @@ public class AdoptionCreator {
     public static Adoption returnValidAdoption() {
         return Adoption.builder()
                 .id(1L)
-                .uuid(UUID.randomUUID())
+                .uuid(UUID.randomUUID().toString())
                 .adopted(PostsCreator.returnValidPosts())
                 .adopter(UserCreator.returnValidUser())
-                .createdAt(Date.from(Instant.now()))
-                .updatedAt(Date.from(Instant.now()))
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
                 .build();
     }
 }
